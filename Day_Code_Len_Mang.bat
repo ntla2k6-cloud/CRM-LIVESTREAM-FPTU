1ec471d22f109f741a1857c5b7b5c7e5a41b902d
ntla2 <ntla2@example.com>
2026-09-19 16:18:53 +0700

    Auto update code

---
+++ Day_Code_Len_Mang.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ==============================================
+echo DANG TIEN HANH DAY CODE LEN GITHUB...
+echo ==============================================
+
+git add .
+git commit -m "Auto update code"
+git push origin main
+
+echo.
+echo ==============================================
+echo HOAN TAT! CODE DA DUOC DAY LEN GITHUB.
+echo VERCEL SE TU DONG CAP NHAT TRONG IT PHUT TOI.
+echo ==============================================
+pause
