e261d94cb5e028ebd56ab61899f72798092e26d3
ntla2 <ntla2@example.com>
2026-09-23 18:59:08 +0700

    feat(auth): force Google to always prompt for account selection

--- Users/ntla2/Downloads/Web_LiveStream/frontend/src/app/api/auth/[...nextauth]/route.ts
+++ Users/ntla2/Downloads/Web_LiveStream/frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,11 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "select_account"
+        }
+      }
     }),
   ],
   pages: {
